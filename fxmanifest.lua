58fb0a4c8df1cfb423a8dac019377a175c4c8a2e
cmd <felixbusiness332000@gmail.com>
2023-03-07 18:14:17 +0100

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'adamant'
+games { 'gta5' }
+
+name 'Core System'
+author 'BSC Studios - BeansFL and cmd'
+version '1.0'
+lua54 'yes'
+
+client_script 'client.lua'
+server_script 'server.lua'
+shared_script 'config.lua' 
